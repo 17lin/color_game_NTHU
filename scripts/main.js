c208dea4900a61c5127864f5a6698b22a9a349b5
unknown <ga850309@gmail.com>
2017-03-21 00:57:55 +0800

    v2

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,21 @@ window.onload = function() {
     init();
 };
 
-var numCards = 3;
+var numCards = 3;       // how many card
 var gameOver = false;
 var colors = [];
 var pickedColor;
 var body = document.querySelector("body");
 var cards = document.querySelectorAll(".card");
-var colorDisplay = document.getElementById("color-picked");
+var colorDisplay = document.getElementById("color-picked"); //question
 var messageDisplay = document.querySelector("#message");
-var h1 = document.querySelector("h1");
+var h1 = document.querySelector("h1"); // word to show in screen
 var resetButton = document.querySelector("#reset");
 var resetDisplay = document.querySelector("#reset span");
 
-function init() {
+function init() {   // initial condition
     initCards();
-    reset();
+    reset();  // first time give color
 }
 
 function initCards() {
@@ -24,19 +24,19 @@ function initCards() {
         //add click listeners to cards
         cards[i].addEventListener("click", function() {
             if (gameOver)
-                return;
+                return;  // no use if click one more time
             //grab color of clicked card
-            var clickedColor = this.style.backgroundColor;
+            var clickedColor = this.style.backgroundColor; // cards[i]
             // alert(this.style.backgroundColor);
             //compare color to pickedColor
             if (clickedColor === pickedColor) {
                 messageDisplay.textContent = "Correct!";
-                resetDisplay.textContent = "Play Again"
+                resetDisplay.textContent = "Play Again" // change the word in the button
                 changeColors("#FFF");
                 body.style.backgroundColor = clickedColor;
                 gameOver = true;
             } else {
-                this.style.opacity = 0;
+                this.style.opacity = 0;  // card[i] become invisible
                 messageDisplay.textContent = "Try Again"
             }
         });
@@ -47,7 +47,7 @@ function reset() {
     gameOver = false;
     colors = generateRandomColors(numCards);
     //pick a new random color from array
-    pickedColor = pickColor();
+    pickedColor = pickColor(); // pick one for correct answer
     //change colorDisplay to match picked Color
     colorDisplay.textContent = pickedColor;
     resetDisplay.textContent = "New Color"
@@ -73,13 +73,13 @@ function changeColors(color) {
     //loop through all cards
     for (var i = 0; i < cards.length; i++) {
         //change each color to match given color
-        cards[i].style.opacity = 1;
+        cards[i].style.opacity = 1;  // transparency
         cards[i].style.backgroundColor = color;
     }
 }
 
 function pickColor() {
-    var random = Math.floor(Math.random() * colors.length);
+    var random = Math.floor(Math.random() * colors.length); // get rid of floating point
     return colors[random];
 }
 
