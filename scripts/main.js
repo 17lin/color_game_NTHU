d86a6b196a9b0a35c7dcfacd1a466e5ebb08b390
Steven <stevenlin0918@gmail.com>
2017-03-21 17:13:08 +0800

    ver 1

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,39 @@ window.onload = function() {
     init();
 };
 
-var numCards = 3;
+var numCards = 6;
 var gameOver = false;
 var colors = [];
 var pickedColor;
 var body = document.querySelector("body");
 var cards = document.querySelectorAll(".card");
+var card_h = document.querySelectorAll(".card_h");
 var colorDisplay = document.getElementById("color-picked");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var resetDisplay = document.querySelector("#reset span");
+var mode;
+var easy = document.querySelector("#easy");
+var hard = document.querySelector("#hard");
 
 function init() {
+    initbtn();
     initCards();
     reset();
 }
-
+function initbtn(){
+    easy.addEventListener("click",function(){
+      mode = "easy";
+      easy.style.backgroundColor = "steelblue";
+    });
+    hard.addEventListener("click",function(){
+      mode = "hard";
+      hard.style.backgroundColor = "steelblueF";
+    });
+}
 function initCards() {
+
     for (var i = 0; i < cards.length; i++) {
         //add click listeners to cards
         cards[i].addEventListener("click", function() {
@@ -41,6 +56,28 @@ function initCards() {
             }
         });
     }
+    for (var i = 0; i < card_h.length; i++) {
+        //add click listeners to cards
+        card_h[i].addEventListener("click", function() {
+            if (gameOver)
+                return;
+            //grab color of clicked card
+            var clickedColor = this.style.backgroundColor;
+            // alert(this.style.backgroundColor);
+            //compare color to pickedColor
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = "Correct!";
+                resetDisplay.textContent = "Play Again"
+                changeColors("#FFF");
+                body.style.backgroundColor = clickedColor;
+                gameOver = true;
+            } else {
+                this.style.opacity = 0;
+                messageDisplay.textContent = "Try Again"
+            }
+        });
+    }
+
 }
 
 function reset() {
@@ -62,6 +99,15 @@ function reset() {
             cards[i].style.display = "none";
         }
     }
+    for (var i = 0; i < card_h.length; i++) {
+        card_h[i].style.opacity = 1;
+        if (colors[i+3]) {
+            card_h[i].style.display = "block"
+            card_h[i].style.backgroundColor = colors[i+3];
+        } else {
+            card_h[i].style.display = "none";
+        }
+    }
     body.style.backgroundColor = "#232323";
 }
 
@@ -75,6 +121,11 @@ function changeColors(color) {
         //change each color to match given color
         cards[i].style.opacity = 1;
         cards[i].style.backgroundColor = color;
+    }
+    for (var i = 0; i < card_h.length; i++) {
+        //change each color to match given color
+        card_h[i].style.opacity = 1;
+        card_h[i].style.backgroundColor = color;
     }
 }
 
