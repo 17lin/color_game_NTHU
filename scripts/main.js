a8c722b985c588b97507b305bb946b5494a0860a
104062135 <devil0939575009@yahoo.com.tw>
2017-03-21 16:47:48 +0800

    Finish lab-js-color-game-quiz

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ window.onload = function() {
 };
 
 var numCards = 3;
+var mode=1;
 var gameOver = false;
 var colors = [];
 var pickedColor;
@@ -14,11 +15,16 @@ var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var resetDisplay = document.querySelector("#reset span");
 
+var mode1Button = document.querySelector("#mode1");
+var mode2Button = document.querySelector("#mode2");
+var mode3Button = document.querySelector("#mode3");
+var timer = document.querySelector("#Timer");
+var num=5;
+var id;
 function init() {
     initCards();
     reset();
 }
-
 function initCards() {
     for (var i = 0; i < cards.length; i++) {
         //add click listeners to cards
@@ -32,6 +38,10 @@ function initCards() {
             if (clickedColor === pickedColor) {
                 messageDisplay.textContent = "Correct!";
                 resetDisplay.textContent = "Play Again"
+                resetDisplay.style.opacity=1;
+                resetButton.style.opacity=1;
+                timer.textContent='';
+                clearInterval(id);
                 changeColors("#FFF");
                 body.style.backgroundColor = clickedColor;
                 gameOver = true;
@@ -44,6 +54,8 @@ function initCards() {
 }
 
 function reset() {
+  num=5;
+  clearInterval(id);
     gameOver = false;
     colors = generateRandomColors(numCards);
     //pick a new random color from array
@@ -67,7 +79,101 @@ function reset() {
 
 resetButton.addEventListener("click", function() {
     reset();
+    if(mode==3){
+    time();
+   }
 })
+
+
+
+
+if(mode==1){
+  mode1Button.style.backgroundColor= 'blue';
+  mode2Button.style.backgroundColor= 'white';
+  mode3Button.style.backgroundColor= 'white';
+}
+function mode1(){
+  numCards=3;
+  mode=1;
+  reset();
+  mode1Button.style.backgroundColor= 'blue';
+  mode2Button.style.backgroundColor= 'white';
+  mode3Button.style.backgroundColor= 'white';
+}
+function mode2(){
+  mode=2;
+  numCards=6;
+  reset();
+  mode1Button.style.backgroundColor= 'white';
+  mode2Button.style.backgroundColor= 'blue';
+  mode3Button.style.backgroundColor= 'white';
+}
+function mode3(){
+  mode=3;
+  numCards=6;
+  reset();
+  mode1Button.style.backgroundColor= 'white';
+  mode2Button.style.backgroundColor= 'white';
+  mode3Button.style.backgroundColor= 'blue';
+}
+
+function time(){
+  timer.textContent=num;
+  if(num>0){
+  num--;
+  if(mode==3){
+  resetDisplay.style.opacity=0;
+  resetButton.style.opacity=0;
+}
+  id = window.setInterval(countdownfunc,1000);
+  }
+  else {
+    clearInterval(id);
+  }
+}
+function countdownfunc(){
+  timer.textContent=num;
+  if (num==0){
+    gameover();
+    clearInterval(id);
+    timer.textContent='';
+  }
+  if(num>0)
+  num--;
+}
+function gameover(){
+  resetDisplay.style.opacity=1;
+  resetButton.style.opacity=1;
+  messageDisplay.textContent = "Time out!!";
+  resetDisplay.textContent = "Play Again"
+  changeColors("#FFF");
+  body.style.backgroundColor = pickedColor;
+  gameOver = true;
+}
+
+mode1Button.addEventListener("click", function() {
+  mode1();
+  timer.textContent='';
+  resetDisplay.style.opacity=1;
+  resetButton.style.opacity=1;
+})
+mode2Button.addEventListener("click", function() {
+   mode2();
+   timer.textContent='';
+   resetDisplay.style.opacity=1;
+   resetButton.style.opacity=1;
+})
+mode3Button.addEventListener("click", function() {
+  mode3();
+   time();
+})
+
+
+
+
+
+
+
 
 function changeColors(color) {
     //loop through all cards
@@ -94,7 +200,6 @@ function generateRandomColors(num) {
     //return that array
     return arr;
 }
-
 function randomColor() {
     //pick a "red" from 0 - 255
     var r = Math.floor(Math.random() * 256);
