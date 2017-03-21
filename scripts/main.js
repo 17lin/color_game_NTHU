936c71af4ea49ec31fb27249f974a6fd4bf175a5
CHINAN CHENG <CHINAN CHENG>
2017-03-21 20:50:31 +0800

    version 3

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ window.onload = function() {
 };
 
 var mode = 0;
+var blink1;
+var count;
 var numCards = 3;
 var gameOver = false;
 var colors = [];
@@ -18,6 +20,12 @@ var resetButton = document.querySelector("#reset");
 var resetDisplay = document.querySelector("#reset span");
 
 function init() {
+  modes[0].style.backgroundColor = "steelblue";
+  modes[0].style.color = "white";
+  modes[1].style.backgroundColor = "white";
+  modes[1].style.color = "#484848";
+  modes[2].style.backgroundColor = "white";
+  modes[2].style.color = "#484848";
     initCards();
     reset();
 }
@@ -40,8 +48,8 @@ function initCards() {
                 gameOver = true;
             } else {
                 this.style.opacity = 0;
-                if (mode!=2)
-                  messageDisplay.textContent = "Try Again"
+                if (mode != 2)
+                    messageDisplay.textContent = "Try Again"
             }
         });
     }
@@ -50,6 +58,7 @@ function initCards() {
 function reset() {
 
     gameOver = false;
+
     colors = generateRandomColors(numCards);
     //pick a new random color from array
     pickedColor = pickColor();
@@ -71,33 +80,50 @@ function reset() {
 }
 
 function easy() {
-      mode = 0;
-      numCards = 3;
-      reset();
-
-}
-
-function hard(){
-    mode = 1;
-    numCards = 6;
+    mode = 0;
+    numCards = 3;
+    modes[0].style.backgroundColor = "steelblue";
+    modes[0].style.color = "white";
+    modes[1].style.backgroundColor = "white";
+    modes[1].style.color = "#484848";
+    modes[2].style.backgroundColor = "white";
+    modes[2].style.color = "#484848";
     reset();
 
 }
 
-function nightmare(){
-  mode = 2;
-  numCards = 6;
-  reset();
-  countdown(6);
+function hard() {
+    mode = 1;
+    numCards = 6;
+    modes[1].style.backgroundColor = "steelblue";
+    modes[1].style.color = "white";
+    modes[2].style.backgroundColor = "white";
+    modes[2].style.color = "#484848";
+    modes[0].style.backgroundColor = "white";
+    modes[0].style.color = "#484848";
+    reset();
+
+}
+
+function nightmare() {
+    mode = 2;
+    numCards = 6;
+    modes[2].style.backgroundColor = "steelblue";
+    modes[2].style.color = "white";
+    modes[1].style.backgroundColor = "white";
+    modes[1].style.color = "#484848";
+    modes[0].style.backgroundColor = "white";
+    modes[0].style.color = "#484848";
+    reset();
+    countdown(6);
 }
 
 resetButton.addEventListener("click", function() {
-    if(mode==2){
-      clearInterval(count);
-      nightmare();
-    }
-    else{
-      reset();
+    if (mode == 2) {
+        clearInterval(count);
+        nightmare();
+    } else {
+        reset();
     }
 })
 modes[0].addEventListener("click", function() {
@@ -118,27 +144,41 @@ modes[2].addEventListener("click", function() {
 
 
 })
+function blink() {
+    body.style.backgroundColor = "white"
+    blink1=setTimeout(function() {
+      body.style.backgroundColor = "#232323";
+    }, 100);
 
-function countdown(time){
-  if(mode==2){
-  count = setInterval(function(){
-    time =time-1;
-    messageDisplay.textContent = "What's the Color? " +time;
-    if (mode!=2)
-      clearInterval(count);
-    else if (gameOver == true){
-        clearInterval(count);
-        messageDisplay.textContent = "Correct!";
-    }
-    else if(time == 0){
-      messageDisplay.textContent = "Timeout!";
-      changeColors("#FFF");
-      body.style.backgroundColor = pickedColor;
-      gameOver = true;
-      clearInterval(count);
+}
+function countdown(time) {
+    if (mode == 2) {
+        count = setInterval(function() {
+          time = time - 1;
+          blink();
+          messageDisplay.textContent = "What's the Color? " + time;
+            if (mode != 2){
+                clearInterval(blink1);
+                clearInterval(count);
+            }
+            else if (gameOver == true) {
+                clearInterval(blink1);
+                clearInterval(count);
+                messageDisplay.textContent = "Correct!";
+                body.style.backgroundColor = pickedColor;
+            } else if (time == 0) {
+                messageDisplay.textContent = "Timeout!";
+                clearInterval(blink1);
+                clearInterval(count);
+                gameOver = true;
+                changeColors("#FFF");
+                body.style.backgroundColor = pickedColor;
+            }
+
+
+        }, 1000);
     }
 
-    }, 1000);}
 
 }
 
