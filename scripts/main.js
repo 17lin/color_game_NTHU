604592051e8d9514d0aeb19dd2b9548a1c664451
104062337 <david80531@gmail.com>
2017-03-21 23:05:00 +0800

    66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points 66points

--- scripts/main.js
+++ scripts/main.js
@@ -13,14 +13,39 @@ var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var resetDisplay = document.querySelector("#reset span");
+var invisible = document.querySelectorAll(".downRow");
+var easy = document.getElementById("easy");
+var hard = document.getElementById("hard");
+var nightmare = document.getElementById("nightmare");
+var timeDisplay = document.getElementById("time");
+var countdownnumber = 5;
+var cdId;
+var nightMode = false;
+var blinkId;
+
 
 function init() {
-    initCards();
+    initCardsEasy();
     reset();
 }
 
-function initCards() {
-    for (var i = 0; i < cards.length; i++) {
+function initCardsHard(){
+    numCards = 6;
+    reset();
+    nightMode = false;
+    hard.style.backgroundColor = "#007FFF";
+    hard.style.color = "#F7F7F7";
+    easy.style.backgroundColor = "#F7F7F7";
+    easy.style.color = "#000";
+    nightmare.style.backgroundColor = "#F7F7F7";
+    nightmare.style.color = "#000";
+    resetButton.style.display = "block";
+    time.style.display = "none";
+    clearInterval(cdId);
+     for(var j = 0;j < 3; j++){
+        invisible[j].style.display = "block";
+    }
+     for (var i = 0; i < 6; i++) {
         //add click listeners to cards
         cards[i].addEventListener("click", function() {
             if (gameOver)
@@ -41,6 +66,122 @@ function initCards() {
             }
         });
     }
+
+}
+
+function initCardsEasy() {
+    numCards = 3;
+    reset();
+    nightMode = false;
+    easy.style.backgroundColor = "#007FFF";
+    easy.style.color = "#F7F7F7";
+    hard.style.backgroundColor = "#F7F7F7";
+    hard.style.color = "#000";
+    nightmare.style.backgroundColor = "#F7F7F7";
+    nightmare.style.color = "#000";
+    resetButton.style.display = "block";
+    time.style.display = "none";
+    clearInterval(cdId);
+    for(var j = 0;j < 3; j++){
+        invisible[j].style.display = "none";
+    }
+    for (var i = 0; i < 3; i++) {
+        //add click listeners to cards
+        cards[i].addEventListener("click", function() {
+            if (gameOver)
+                return;
+            //grab color of clicked card
+            var clickedColor = this.style.backgroundColor;
+            // alert(this.style.backgroundColor);
+            //compare color to pickedColor
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = "Correct!";
+                resetDisplay.textContent = "Play Again"
+                changeColors("#FFF");
+                body.style.backgroundColor = clickedColor;
+                gameOver = true;
+            } else {
+                this.style.opacity = 0;
+                messageDisplay.textContent = "Try Again"
+            }
+        });
+    }
+}
+function initCardsNight(){
+    numCards = 6;
+    reset();
+    clearInterval(cdId);
+    nightMode = true;
+    resetButton.style.display = "none";
+    nightmare.style.backgroundColor = "#007FFF";
+    nightmare.style.color = "#F7F7F7";
+    easy.style.backgroundColor = "#F7F7F7";
+    easy.style.color = "#000";
+    hard.style.backgroundColor = "#F7F7F7";
+    hard.style.color = "#000";
+    time.style.display = "inline";
+    for(var j = 0;j < 3; j++){
+        invisible[j].style.display = "block";
+    }
+
+    initcountdown();
+
+     for (var i = 0; i < 6; i++) {
+        //add click listeners to cards
+        cards[i].addEventListener("click", function() {
+        if (gameOver)
+            return;
+            //grab color of clicked card
+        var clickedColor = this.style.backgroundColor;
+            // alert(this.style.backgroundColor);
+            //compare color to pickedColor
+        if (clickedColor === pickedColor) {
+            clearInterval(cdId);
+            messageDisplay.textContent = "Correct!";
+            resetButton.style.display = "block";
+            resetDisplay.textContent = "Play Again";
+            time.style.display = "none";
+            changeColors("#FFF");
+            body.style.backgroundColor = clickedColor;
+            gameOver = true;
+        } else {
+            this.style.opacity = 0;
+            messageDisplay.textContent = "Try Again";
+            }
+        });
+    }
+}
+
+function initcountdown(){
+    countdownnumber = 5;
+    time.textContent = countdownnumber;
+    countdownnumber--;
+    cdId = setInterval(cdfunc, 1000);
+}
+
+function cdfunc(){
+    time.textContent = countdownnumber;
+    body.style.backgroundColor = "#aaaaaa";
+    if(countdownnumber === 0){
+        if(!gameOver){
+            time.style.display = "none";
+            messageDisplay.textContent = "Timeout!";
+            resetButton.style.display = "block";
+            resetDisplay.textContent = "Play Again";
+            changeColors("#FFF");
+            body.style.backgroundColor = pickedColor;
+        }
+            clearInterval(cdId);
+            clearInterval(blinkId);
+            return;
+    } else {
+       blinkId = setInterval(function(){
+           body.style.backgroundColor ="#232323";
+           clearInterval(blinkId);
+       }, 300);    
+   
+    }
+    countdownnumber--;
 }
 
 function reset() {
@@ -66,7 +207,8 @@ function reset() {
 }
 
 resetButton.addEventListener("click", function() {
-    reset();
+    if(nightMode) initCardsNight();
+    else reset();
 })
 
 function changeColors(color) {
